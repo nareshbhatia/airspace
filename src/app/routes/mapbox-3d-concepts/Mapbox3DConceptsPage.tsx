23c5950d1220e56d2a27c29722ee23900da438d0
Naresh Bhatia <me@nareshbhatia.dev>
2026-04-01 23:12:08 -0700

    Add PitchToggle (#50)

--- src/app/routes/mapbox-3d-concepts/Mapbox3DConceptsPage.tsx
+++ src/app/routes/mapbox-3d-concepts/Mapbox3DConceptsPage.tsx
@@ -7,11 +7,56 @@ import {
   MapPanel,
   MapProvider,
   PitchDisplay,
+  PitchToggle,
   SceneSelector,
   TerrainSwitch,
+  useMapPitch3dToggle,
   ZoomLevelDisplay,
 } from '../../../lib/mapbox';
 import { cn } from '../../../utils/cn';
+
+import type { AirspaceScene } from '../../../lib/mapbox/types/AirspaceScene';
+
+interface Mapbox3DConceptsPanelProps {
+  isTerrainEnabled: boolean;
+  onTerrainEnabledChange: (isTerrainEnabled: boolean) => void;
+  is3dEnabled: boolean;
+  on3dEnabledChange: (is3dEnabled: boolean) => void;
+  scene: AirspaceScene;
+  onSceneChange: (scene: AirspaceScene) => void;
+}
+
+function Mapbox3DConceptsPanel({
+  isTerrainEnabled,
+  onTerrainEnabledChange,
+  is3dEnabled,
+  on3dEnabledChange,
+  scene,
+  onSceneChange,
+}: Mapbox3DConceptsPanelProps) {
+  useMapPitch3dToggle(is3dEnabled);
+
+  return (
+    <MapPanel className="absolute right-3 top-3 z-10">
+      <ZoomLevelDisplay />
+      <PitchDisplay />
+      <BearingDisplay />
+      <TerrainSwitch
+        isTerrainEnabled={isTerrainEnabled}
+        onTerrainEnabledChange={onTerrainEnabledChange}
+      />
+      <PitchToggle
+        is3dEnabled={is3dEnabled}
+        on3dEnabledChange={on3dEnabledChange}
+      />
+      <SceneSelector
+        scenes={scenes}
+        selectedScene={scene}
+        onSceneChange={onSceneChange}
+      />
+    </MapPanel>
+  );
+}
 
 /**
  * Minimal Mapbox page used for 3D concepts exploration.
@@ -19,6 +64,7 @@ import { cn } from '../../../utils/cn';
 export function Mapbox3DConceptsPage() {
   const [scene, setScene] = useState(DEFAULT_SCENE);
   const [isTerrainEnabled, setIsTerrainEnabled] = useState(true);
+  const [is3dEnabled, setIs3dEnabled] = useState(true);
 
   return (
     <div
@@ -35,20 +81,14 @@ export function Mapbox3DConceptsPage() {
           mapOptions={{ antialias: true }}
           enableTerrain={isTerrainEnabled}
         >
-          <MapPanel className="absolute right-3 top-3 z-10">
-            <ZoomLevelDisplay />
-            <PitchDisplay />
-            <BearingDisplay />
-            <TerrainSwitch
-              isTerrainEnabled={isTerrainEnabled}
-              onTerrainEnabledChange={setIsTerrainEnabled}
-            />
-            <SceneSelector
-              scenes={scenes}
-              selectedScene={scene}
-              onSceneChange={setScene}
-            />
-          </MapPanel>
+          <Mapbox3DConceptsPanel
+            isTerrainEnabled={isTerrainEnabled}
+            onTerrainEnabledChange={setIsTerrainEnabled}
+            is3dEnabled={is3dEnabled}
+            on3dEnabledChange={setIs3dEnabled}
+            scene={scene}
+            onSceneChange={setScene}
+          />
         </MapProvider>
       </div>
     </div>
